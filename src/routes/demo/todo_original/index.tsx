0be21f519172f54c9e7f5b525bc27656ead71bf7
lightbluepoppy <65863981+lightbluepoppy@users.noreply.github.com>
2023-12-11 15:28:24 +0100

    feat(Backend): :sparkles: add database task handler

--- src/routes/demo/todo_original/index.tsx
+++ src/routes/demo/todo_original/index.tsx
@@ -1,4 +1,7 @@
 import { component$ } from "@builder.io/qwik"
+import { server$ } from "@builder.io/qwik-city"
+import { db } from "db/server"
+import { task } from "db/schema/schema"
 import {
     type DocumentHead,
     routeLoader$,
@@ -9,48 +12,60 @@ import {
 } from "@builder.io/qwik-city"
 import styles from "./todolist.module.css"
 
-interface ListItem {
+type ClientTaskListItem = {
     text: string
 }
 
-export const list: ListItem[] = []
+export const clientTaskList: ClientTaskListItem[] = []
 
-export const useListLoader = routeLoader$(() => {
-    return list
+export const useClientTaskListLoader = routeLoader$(() => {
+    return clientTaskList
 })
 
-export const useAddToListAction = routeAction$(
-    (item) => {
-        list.push(item)
-        return {
-            success: true,
-        }
+export const useServerTaskListLoader = routeLoader$(() => {
+    const serverTaskList = db.query.task.findMany()
+    return serverTaskList
+})
+
+export const useAddToTaskListAction = routeAction$(
+    async (item) => {
+        clientTaskList.push(item)
+        const result = await db.insert(task).values({ title: item.text }).returning()
+        console.log(result)
+        // return {
+        //     success: true,
+        // }
     },
     zod$({
         text: z.string().trim().min(1),
     }),
 )
 
+// export const useAddToServerTaskListAction = server$((item) => {
+//     const result = db.insert(task).values({ title: item.title }).returning()
+//     console.log(result)
+// })
+
 export default component$(() => {
-    const list = useListLoader()
-    const action = useAddToListAction()
+    const taskList = useClientTaskListLoader()
+    const action = useAddToTaskListAction()
 
     return (
         <>
             <div class="container-center container">
                 <h1>
-                    <span class="highlight">TODO</span> List
+                    <span class="highlight">TODO</span> List Original
                 </h1>
             </div>
 
             <div role="presentation" class="ellipsis"></div>
 
             <div class="container-center container">
-                {list.value.length === 0 ? (
+                {taskList.value.length === 0 ? (
                     <span class={styles.empty}>No items found</span>
                 ) : (
                     <ul class={styles.list}>
-                        {list.value.map((item, index) => (
+                        {taskList.value.map((item, index) => (
                             <li key={`items-${index}`}>{item.text}</li>
                         ))}
                     </ul>
